0fecc26f0ccaad18f233c176fb54c315ecbbc727
amtoaer <amtoaer@outlook.com>
2020-10-12 21:30:21 +0800

    fix: 修复起始点位于右侧或上侧时导致的绘图错位问题

--- src/Drawboard.java
+++ src/Drawboard.java
@@ -5,7 +5,6 @@ import java.awt.*;
 
 public class Drawboard extends JPanel {
     static final long serialVersionUID = 1357997531;
-    private final JFrame window = new JFrame();
 
     public Drawboard() {
         this.setBackground(Color.WHITE);
@@ -14,6 +13,7 @@ public class Drawboard extends JPanel {
     }
 
     private void drawUI() {
+        JFrame window = new JFrame();
         window.setSize(800, 600);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setLayout(new BorderLayout());
--- src/MultiShape.java
+++ src/MultiShape.java
@@ -7,15 +7,26 @@ public class MultiShape extends Shape {
 
     public void draw(Graphics p) {
         p.setColor(this.selectedColor);
+        // 始终保证x1<=x2,y1<=y2
+        if (x1 > x2) {
+            int tmp = x1;
+            x1 = x2;
+            x2 = tmp;
+        }
+        if (y1 > y2) {
+            int tmp = y1;
+            y1 = y2;
+            y2 = tmp;
+        }
         switch (this.operation) {
             case "铅笔":
             case "直线":
                 p.drawLine(x1, y1, x2, y2);
                 break;
             case "矩形":
-                p.drawRect(x1, y1, Math.abs(x1 - x2), Math.abs(y1 - y2));
+                p.drawRect(x1, y1, x2 - x1, y2 - y1);
             case "圆":
-                p.drawOval(x1, y1, Math.abs(x1 - x2), Math.abs(y1 - y2));
+                p.drawOval(x1, y1, x2 - x1, y2 - y1);
         }
     }
 }
